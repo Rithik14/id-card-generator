31f3fa7f74ffbbeadce06a13ba508e82f6d32224
Rithik k <rithikkumble@gmail.com>
2024-03-13 00:30:16 +0530

    Add files via upload

---
+++ database/ccbd_id_card_genarator.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2024 at 07:56 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ccbd_id_card_genarator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cards`
+--
+
+CREATE TABLE `cards` (
+  `sno` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `grade` varchar(3) NOT NULL,
+  `id_no` varchar(15) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `dob` varchar(20) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `exp_date` varchar(20) DEFAULT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cards`
+--
+
+INSERT INTO `cards` (`sno`, `name`, `grade`, `id_no`, `email`, `phone`, `address`, `dob`, `date`, `exp_date`, `image`) VALUES
+(1, 'Rithik k', '', '4CB20IS042', 'rithikkumble@gmail.com]', '06282711688', 'kumble', '2024-03-12', '2024-03-11 23:18:19', '2024-03-13', 'assets/uploads/photo[rithik].jpg'),
+(2, 'Rithik Kumble', '', '4CB20IS040', 'ravi@gmail.com]', '06282711688', 'Durga Kripa house chc road kumble', '2024-03-14', '2024-03-13 00:12:03', '2024-03-15', 'assets/uploads/profilepic.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
